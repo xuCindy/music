4a19195192c9090644c22db0c9a3554481eb67f1
cindyXu <serinaxu@163.com>
2018-10-11 13:23:03 +0800

    tab组件

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,16 +21,15 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    'eol-last': 0,
-    'no-tabs': 0,
-    'indent': 0,
+    'space-before-function-paren': 0,
     'no-multiple-empty-lines': 0,
-    'space-before-function-paren': 0
+    'eol-last': 0,
+    'indent': 0
   }
 }
